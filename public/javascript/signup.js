c614b6ea5ba8d91f0e395ce8cdae79ccee7f1282
Maia Davis <maia@Maias-Air.lan>
2022-08-03 17:52:37 -0500

    created views for login, signup, and homepage. created file structure for mvc

---
+++ public/javascript/signup.js
@@ -0,0 +1,17 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users'), {
+            method: 'POST',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type: 'application/json' }
+        });
+    }
+}
