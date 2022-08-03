ac93ae42edb8ed20de10fe2d56832a867cec3060
Maia Davis <maia@Maias-Air.lan>
2022-08-03 18:12:07 -0500

    post and comment models created

---
+++ models/Comment.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const { canTreatArrayAsAnd } = require('sequelize/types/utils');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            validate: {
+                len: [3]
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
