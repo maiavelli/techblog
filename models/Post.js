8ccb51873cde63a8b8df10b78d72f61e16507532
Maia Davis <maia@Maias-Air.lan>
2022-08-04 14:43:17 -0500

    edit post handlebars written

--- models/Post.js
+++ models/Post.js
@@ -15,7 +15,7 @@ Post.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        post_body: {
+        body: {
             type: DataTypes.TEXT,
             allowNull: false
         },
